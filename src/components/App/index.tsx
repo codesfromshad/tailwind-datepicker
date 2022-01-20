f79cd00b0747a5db4542dd67e737287b142e1fec
codesfromshad <contact@notesfromshad.com>
2022-01-21 02:36:14 +0600

    Add functions for proper rendering of components

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -11,6 +11,14 @@ function App() {
 
   const calendarDays = Calendar;
 
+  const weekNumber = new Set();
+  
+  const filteredCalendarWeekNumbers = calendarDays.filter((obj) => {
+    const isPresentInSet = weekNumber.has(obj.date.weekNumber);
+    weekNumber.add(obj.date.weekNumber);
+    return !isPresentInSet;
+  });
+
   const nameOfDays = [
     {
       id: 0,
@@ -79,172 +87,150 @@ function App() {
 
 
   return (
-    <div className="flex items-center justify-center w-full min-h-screen bg-gray-50">
-      <div className="select-none flex bg-white shadow-lg rounded-xl w-full sm:w-auto justify-center">
-        <div className="flex flex-col w-[360px] justify-evenly px-5 pt-5 pb-6 border-b border-gray-100">
-          <div className="flex items-center justify-between">
-            <button className="text-lg text-gray-700 font-semibold rounded-md cursor-pointer hover:bg-gray-50 px-3 py-2">
-              September 2021
-            </button>
-            <div className="flex justify-center space-x-2">
-              <button
-                className="inline-flex items-center p-1.5 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 g-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                <ChevronLeftIcon className="w-6 h-6 stroke-current"/>
+    <div className="dark">
+      <div className="flex items-center justify-center w-full min-h-screen bg-gray-50 dark:bg-slate-900">
+        <div className="select-none flex bg-white dark:bg-gray-800 shadow-lg rounded-xl w-full sm:w-auto justify-center">
+          <div className="flex flex-col w-[360px] justify-evenly px-5 pt-5 pb-6 border-b rounded-xl border-gray-100 dark:border-slate-800">
+            <div className="flex items-center justify-between">
+              <button className="text-lg text-gray-700 dark:text-slate-200 font-semibold rounded-md cursor-pointer hover:bg-gray-50 dark:hover:bg-slate-700 px-3 py-2 active:bg-gray-100 dark:active:bg-slate-600">
+                September 2021
               </button>
-              <button
-                className="inline-flex items-center p-1.5 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 g-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                <ChevronRightIcon className="w-6 h-6 stroke-current"/>
-              </button>
+              <div className="flex justify-center space-x-2">
+                <button
+                  className="inline-flex items-center p-1.5 border border-gray-300 dark:border-slate-200 shadow-sm dark:shadow-slate-900 text-sm leading-4 font-medium rounded-md text-gray-700 dark:text-slate-200 g-white hover:bg-gray-50 dark:hover:bg-slate-700 active:bg-gray-100 dark:active:bg-slate-600"
+                >
+                  <ChevronLeftIcon className="w-6 h-6 stroke-current"/>
+                </button>
+                <button
+                  className="inline-flex items-center p-1.5 border border-gray-300 dark:border-slate-200 shadow-sm dark:shadow-slate-900 text-sm leading-4 font-medium rounded-md text-gray-700 dark:text-slate-200 g-white hover:bg-gray-50 dark:hover:bg-slate-700 active:bg-gray-100 dark:active:bg-slate-600"
+                >
+                  <ChevronRightIcon className="w-6 h-6 stroke-current"/>
+                </button>
+              </div>
             </div>
-          </div>
-          <div className="flex justify-around pt-2">
-            <div className="hidden grid grid-rows-6 pt-10 pr-4 gap-y-1.5 text-sm text-gray-300">
-              <span
-                className="flex items-center justify-center w-full h-auto rounded-md"
-              >
-                11
-              </span>
-              <span
-                className="flex items-center justify-center w-full h-auto rounded-md"
-              >
-                12
-              </span>
-              <span
-                className="flex items-center justify-center w-full h-auto rounded-md"
-              >
-                13
-              </span>
-              <span
-                className="flex items-center justify-center w-full h-auto rounded-md"
-              >
-                14
-              </span>
-              <span
-                className="flex items-center justify-center w-full h-auto rounded-md"
-              >
-                15
-              </span>
-              <span
-                className="flex items-center justify-center w-full h-auto rounded-md"
-              >
-                16
-              </span>
-            </div>
-
-            <div className="w-full">
-
-              {/* Calendar Week Elements */}
-
-              <div className="grid grid-cols-7 text-sm text-center text-gray-700">
-                {nameOfDays.map((nameOfDay) => {
+            <div className="flex justify-around pt-2">
+              <div className="grid grid-rows-6 pt-10 pr-4 gap-y-1.5 text-sm text-gray-300 dark:text-slate-600">
+                {filteredCalendarWeekNumbers.map((day) => {
                   return (
                     <div
-                      className={classNames(nameOfDay.isWeekend 
-                        ? "text-red-700"
-                        : null,
-                        "flex items-center justify-center w-full h-10 font-semibold rounded-md"
-                      )}
+                      className="flex items-center justify-center w-full h-auto"
                     >
-                      {nameOfDay.dayName.substring(0,2)}
+                      {day.date.weekNumber}
                     </div>
-                  );
+                  ); 
                 })}
-
               </div>
+              <div className="w-full">
 
-              <div className="grid grid-cols-7 gap-y-1.5 text-sm text-gray-700">
+                {/* Calendar Week Elements */}
 
-                {/* Calendar Days Elements */}
-
-                {calendarDays.map((day) => {
-                  return (
-                    <div className={classNames(
-                      day.isInRange && !(day.isRangeStart || day.isRangeEnd) && day.isStartingDayOfWeek
-                        ? "bg-blue-100"
-                        : day.isInRange && !(day.isRangeStart || day.isRangeEnd) && day.isEndingDayOfWeek
-                          ? "bg-blue-100"
-                          : day.isInRange && !(day.isRangeStart || day.isRangeEnd)
-                            ? "bg-blue-100"
-                            : null,
-                      day.isRangeStart
-                        ? "bg-gradient-to-r from-transparent via-transparent to-blue-100"
-                        : null,
-                      day.isRangeEnd
-                        ? "bg-gradient-to-l from-transparent via-transparent to-blue-100"
-                        : null,
-                    
-                      "flex items-center justify-center w-full"
-                    )}>
-                      <button
-                        className={classNames(
-                          day.isInteractable
-                            ? "cursor-pointer hover:border-gray-300 hover:bg-gray-50 hover:shadow-sm"
-                            : "border-transparent cursor-not-allowed text-gray-300",
-                          day.isFromPreviousMonth
-                            ? "text-gray-300"
-                            : null,
-                          day.isFromNextMonth
-                            ? "text-gray-300"
-                            : null,
-                          (day.isWeekend || day.isHoliday) && !(day.isFromPreviousMonth || day.isFromNextMonth) && !day.isInRange
-                            ? "text-red-700"
-                            : null,
-                          (day.isWeekend || day.isHoliday) && (day.isFromPreviousMonth || day.isFromNextMonth) && !day.isInRange
-                            ? "text-red-300"
-                            : null,
-                          day.isToday
-                            ? (day.isWeekend || day.isHoliday)
-                              ? "rounded-md font-semibold border border-red-300 hover:border-red-500 hover:bg-red-50 shadow-sm shadow-red-100"
-                              : "rounded-md text-blue-700 font-semibold border border-blue-300 hover:border-blue-500 hover:bg-blue-50 shadow-sm shadow-blue-100"
-                            : "border-transparent",
-                          day.isSelected
-                            ? "font-semibold text-white bg-blue-600 hover:border-blue-700 hover:bg-blue-700 shadow-sm shadow-blue-100"
-                            : null,
-                          day.isInRange && !(day.isRangeStart || day.isRangeEnd)
-                            ? "font-semibold text-blue-700 bg-transparent hover:border-blue-500 hover:bg-blue-200 hover:text-blue-700 hover:shadow-sm hover:shadow-blue-100"
-                            : day.isRangeStart
-                              ? "rounded-r-none font-semibold text-white bg-blue-600 border-blue-500 hover:border-blue-700 hover:bg-blue-700 hover:rounded-md shadow-sm shadow-blue-100 transition-all duration-75"
-                              : day.isRangeEnd
-                                ? "rounded-l-none font-semibold text-white bg-blue-600 border-blue-500 hover:border-blue-700 hover:bg-blue-700 hover:rounded-md shadow-sm shadow-blue-100 transition-all duration-75"
-                                : null,
-                    
-                          "flex items-center justify-center w-10 h-10 rounded-md border"
-                    
+                <div className="grid grid-cols-7 text-sm text-center text-gray-700 dark:text-slate-200">
+                  {nameOfDays.map((nameOfDay) => {
+                    return (
+                      <div
+                        className={classNames(nameOfDay.isWeekend
+                          ? "text-red-700 dark:text-red-500"
+                          : null,
+                          "flex items-center justify-center w-full h-10 font-semibold rounded-md"
                         )}
                       >
-                        {day.date.day}
-                      </button>
-                    </div>
-                  );
-                })}
+                        {nameOfDay.dayName.substring(0,2)}
+                      </div>
+                    );
+                  })}
+                </div>
+                <div className="grid grid-cols-7 gap-y-1.5 text-sm text-gray-700 dark:text-slate-200">
+
+                  {/* Calendar Days Elements */}
+
+                  {calendarDays.map((day) => {
+                    return (
+                      <div className={classNames(
+                        day.isInRange && !(day.isRangeStart || day.isRangeEnd) && day.isStartingDayOfWeek
+                          ? "bg-blue-100 dark:bg-slate-900"
+                          : day.isInRange && !(day.isRangeStart || day.isRangeEnd) && day.isEndingDayOfWeek
+                            ? "bg-blue-100 dark:bg-slate-900"
+                            : day.isInRange && !(day.isRangeStart || day.isRangeEnd)
+                              ? "bg-blue-100 dark:bg-slate-900"
+                              : null,
+                        day.isRangeStart
+                          ? "bg-gradient-to-r from-transparent via-transparent to-blue-100 dark:bg-gradient-to-r dark:from-transparent dark:via-transparent dark:to-slate-900"
+                          : null,
+                        day.isRangeEnd
+                          ? "bg-gradient-to-l from-transparent via-transparent to-blue-100 dark:bg-gradient-to-l dark:from-transparent dark:via-transparent dark:to-slate-900"
+                          : null,
+      
+                        "flex items-center justify-center w-full"
+                      )}>
+                        <button
+                          className={classNames(
+                            day.isInteractable
+                              ? "cursor-pointer hover:border-gray-300 dark:hover:border-slate-600 hover:bg-gray-50 dark:hover:bg-slate-800 hover:shadow-sm"
+                              : "border-transparent cursor-not-allowed text-gray-300 dark:text-slate-600",
+                            day.isFromPreviousMonth
+                              ? "text-gray-300 dark:text-slate-600"
+                              : null,
+                            day.isFromNextMonth
+                              ? "text-gray-300 dark:text-slate-600"
+                              : null,
+                            (day.isWeekend || day.isHoliday) && !(day.isFromPreviousMonth || day.isFromNextMonth) && !day.isInRange
+                              ? "text-red-700 dark:text-red-500"
+                              : null,
+                            (day.isWeekend || day.isHoliday) && (day.isFromPreviousMonth || day.isFromNextMonth) && !day.isInRange
+                              ? "text-red-300 dark:text-red-900"
+                              : null,
+                            day.isToday
+                              ? (day.isWeekend || day.isHoliday)
+                                ? "rounded-md font-semibold border border-red-400 dark:border-red-900 hover:border-red-500 dark:hover:border-red-800 hover:bg-red-50 dark:hover:bg-red-900 shadow-sm shadow-red-100 dark:shadow-red-900"
+                                : "rounded-md text-blue-700 dark:text-blue-400 font-semibold border border-blue-400 dark:border-blue-700 hover:border-blue-500 dark:hover:border-blue-600 hover:bg-blue-50 dark:hover:bg-blue-900 shadow-sm shadow-blue-100 dark:shadow-blue-700"
+                              : "border-transparent",
+                            day.isSelected
+                              ? "font-semibold text-white bg-blue-700 hover:bg-blue-600 dark:bg-blue-800  hover:border-blue-500 dark:border-blue-700 dark:hover:border-blue-600 dark:hover:bg-blue-700 shadow-sm shadow-blue-100 dark:shadow-blue-900"
+                              : null,
+                            day.isInRange && !(day.isRangeStart || day.isRangeEnd)
+                              ? "font-semibold text-blue-700 dark:text-blue-400 bg-transparent hover:border-blue-500 hover:bg-blue-200 dark:hover:bg-slate-700 hover:text-blue-700 hover:shadow-sm hover:shadow-blue-100 dark:hover:shadow-slate-900"
+                              : day.isRangeStart
+                                ? "rounded-r-none font-semibold text-white bg-blue-700 dark:bg-blue-800 border-blue-500 dark:border-blue-700 hover:border-blue-700 dark:hover:border-blue-600 hover:bg-blue-600 dark:hover:bg-blue-700 hover:rounded-md shadow-sm shadow-blue-100 dark:shadow-blue-900 transition-all duration-75"
+                                : day.isRangeEnd
+                                  ? "rounded-l-none font-semibold text-white bg-blue-700 dark:bg-blue-800 border-blue-500 dark:border-blue-700 hover:border-blue-700 dark:hover:border-blue-600 hover:bg-blue-600 dark:hover:bg-blue-700 hover:rounded-md shadow-sm shadow-blue-100 dark:shadow-blue-900 transition-all duration-75"
+                                  : null,
+      
+                            "flex items-center justify-center w-10 h-10 rounded-md border"
+      
+                          )}
+                        >
+                          {day.date.day}
+                        </button>
+                      </div>
+                    );
+                  })}
+                </div>
+      
               </div>
-            
             </div>
-          </div>
-          <div className="flex justify-between pt-5">
-            <div className="flex">
-              <button
-                type="button"
-                className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-              >
-                Today
-              </button>
-            </div>
-            <div className="flex space-x-2">
-              <button
-                type="button"
-                className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-              >
-                Cancel
-              </button>
-              <button
-                type="button"
-                className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-700 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-              >
-                Set date
-              </button>
+            <div className="flex justify-between pt-5">
+              <div className="flex">
+                <button
+                  type="button"
+                  className="inline-flex items-center px-4 py-2 border border-gray-300 dark:border-slate-200 shadow-sm text-sm dark:shadow-slate-900 font-medium rounded-md text-gray-700 dark:text-slate-200 bg-white dark:bg-slate-800 hover:bg-gray-50 dark:hover:bg-slate-700 active:bg-gray-100 dark:active:bg-slate-600"
+                >
+                  Today
+                </button>
+              </div>
+              <div className="flex space-x-2">
+                <button
+                  type="button"
+                  className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-blue-700 dark:text-blue-400 bg-blue-100 dark:bg-slate-900 hover:bg-blue-200 dark:hover:bg-slate-700 active:bg-blue-300 active:text-blue-800 dark:active:bg-slate-600"
+                >
+                  Cancel
+                </button>
+                <button
+                  type="button"
+                  className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm dark:shadow-blue-900 text-white bg-blue-700 dark:bg-blue-800 hover:bg-blue-600 dark:hover:bg-blue-700 active:bg-blue-800 dark:active:bg-blue-900"
+                >
+                  Set date
+                </button>
+              </div>
             </div>
           </div>
         </div>
